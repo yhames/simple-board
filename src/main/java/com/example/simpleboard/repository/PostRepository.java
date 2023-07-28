77509e480edda5309c29279b1e215d3abc1aeef0
yhames <wjddn494@naver.com>
2023-07-28 16:47:10 +0900

    PostRepository 인터페이스 분리

--- src/main/java/com/example/simpleboard/repository/PostRepository.java
+++ src/main/java/com/example/simpleboard/repository/PostRepository.java
@@ -1,45 +1,11 @@
 package com.example.simpleboard.repository;
 
 import com.example.simpleboard.domain.Post;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Slf4j
-@Repository
-@RequiredArgsConstructor
-public class PostRepository {
+public interface PostRepository {
 
-    private final NamedParameterJdbcTemplate jdbcTemplate;
+    List<Post> findAll();
 
-    public List<Post> findAll() {
-        String sql = "SELECT p.id, u.name as writer, b.name as board,"
-                + " p.title, p.content, p.created_date, p.updated_date, p.count_of_comments, p.count_of_views"
-                + " FROM POST as p"
-                + " LEFT OUTER JOIN USER as u"
-                + " ON p.user_id = u.id"
-                + " LEFT OUTER JOIN BOARD as b"
-                + " ON p.board_id = b.id";
-        return jdbcTemplate.query(sql, rowMapper());
-    }
-
-    private RowMapper<Post> rowMapper() {
-        return (rs, rowNum) -> {
-            Post post = new Post();
-            post.setId(rs.getLong("id"));
-            post.setWriter(rs.getString("writer"));
-            post.setBoard(rs.getString("board"));
-            post.setTitle(rs.getString("title"));
-            post.setContent(rs.getString("content"));
-            post.setCreatedDate(rs.getTimestamp("created_date").toLocalDateTime());
-            post.setUpdatedDate(rs.getTimestamp("updated_date").toLocalDateTime());
-            post.setCountOfComments(rs.getInt("count_of_comments"));
-            post.setCountOfViews(rs.getInt("count_of_views"));
-            return post;
-        };
-    }
 }
